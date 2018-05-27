ebac5f3a5e4457dd229d405bad8f5c8079f39a55
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2018-05-27 04:21:29 -0300

    Training and test

--- bag_of_visual_words.py
+++ bag_of_visual_words.py
@@ -1,28 +1,35 @@
 import cv2
+import os
 import glob
-import numpy
+import pickle
+import numpy as np
+from sklearn.cluster import KMeans
 
 
-def getFiles(path):
-    imList = {}
-    count = 0
+def getFilesAndDescriptors(path):
+    infoList = []
+    classCount = 0
     surf = cv2.xfeatures2d.SURF_create(400)
 
     for each in glob.glob(path + "*"):
         word = each.split("/")[-1]
         print(" #### Reading image category ", word, " ##### ")
-        imList[word] = []
+
+        classCount += 1
 
         for imagefile in glob.glob(path+word+"/*"):
             im = cv2.imread(imagefile, 0)
             desc = features(surf, im)
 
-            print("Reading file " + imagefile + " - Desc: " + str(len(desc)))
+            if desc is not None:
+                infoList.append((word, desc[:100]))
 
-            imList[word].append((im, desc))
-            count +=1
+    return infoList, classCount
 
-    return imList, count
+
+def extractTuple(tupleList, position):
+    ret = [x[position] for x in tupleList]
+    return ret
 
 
 def features(surf, image):
@@ -30,8 +37,122 @@ def features(surf, image):
     return descriptors
 
 
+def generateCodeBook(descList, classCount):
+    return KMeans(n_clusters=classCount * 50, random_state=0).fit(descList)
+
+
+def makeHistogram(predict, n_clusters):
+    hist = np.zeros(n_clusters)
+
+    for cluster in predict:
+        hist[cluster] += 1
+
+    return hist
+
+
+def calculateHistograms(classList, descList, codeBook):
+    histograms = {}
+
+    for i, category in enumerate(classList):
+        predict = codeBook.predict(descList[i])
+
+        imHist = makeHistogram(predict, codeBook.n_clusters)
+
+        if category in histograms:
+            histograms[category] += imHist
+        else:
+            histograms[category] = imHist
+
+    # Normalize histograms
+    for category in histograms:
+        hist_sum = sum(histograms[category])
+        histograms[category] /= hist_sum
+
+    return histograms
+
+
+def classifyImage(imDesc, codeBook, histograms):
+    imPredict = codeBook.predict(imDesc)
+    imHist = makeHistogram(imPredict, codeBook.n_clusters)
+    greaterSum = 0
+
+    for category in histograms:
+        categorySum = sum(imHist * histograms[category])
+
+        if greaterSum < categorySum:
+            imCategory = category
+            greaterSum = categorySum
+
+    return imCategory
+
+
+def test(codeBook, histograms):
+    testInfoList, _ = getFilesAndDescriptors("Images/Test/")
+
+    testClassList = extractTuple(testInfoList, 0)
+    testDescList = np.array(extractTuple(testInfoList, 1))
+
+    right = 0
+
+    for imDesc, imClass in zip(testDescList, testClassList):
+        category = classifyImage(imDesc, codeBook, histograms)
+        
+        if imClass == category:
+            right += 1
+
+    print('Acertos: ', str(100 * right / len(testClassList)), ' %')
+
+
+def test2(codeBook, histograms):
+    testInfoList, classCount = getFilesAndDescriptors("Images/Test/")
+
+    testClassList = extractTuple(testInfoList, 0)
+    testDescList = np.array(extractTuple(testInfoList, 1))
+
+    histList = []
+    for category in histograms:
+        histList.append(histograms[category])
+
+    kmeans = KMeans(n_clusters=len(histList), random_state=0).fit(histList)
+
+    for imDesc in testDescList:
+        imPredict = codeBook.predict(imDesc)
+        imHist = makeHistogram(imPredict, codeBook.n_clusters)
+
+        i = kmeans.predict([imHist])[0]
+
+        for key, value in histograms.items():
+            if np.array_equal(value, kmeans.cluster_centers_[i]):
+                print(key)
+
+
+
 def main():
-    imTrainList, Traincount = getFiles("Images/Train/")
+    trainInfoList, classCount = getFilesAndDescriptors("Images/Train/")
+
+    trainClassList = extractTuple(trainInfoList, 0)
+    trainDescList = np.array(extractTuple(trainInfoList, 1))
+
+    if os.path.exists('codeBook.pkl'):
+        with open('codeBook.pkl', 'rb') as inputFile:
+            codeBook = pickle.load(inputFile)
+    else:
+        codeBook = generateCodeBook(np.vstack(trainDescList), classCount)
+
+        with open('codeBook.pkl', 'wb') as outputFile:
+            pickle.dump(codeBook, outputFile, pickle.HIGHEST_PROTOCOL)
+
+
+    if os.path.exists('histograms.pkl'):
+        with open('histograms.pkl', 'rb') as inputFile:
+            histograms = pickle.load(inputFile)
+    else:
+        histograms = calculateHistograms(trainClassList, trainDescList, codeBook)
+
+        with open('histograms.pkl', 'wb') as outputFile:
+            pickle.dump(histograms, outputFile, pickle.HIGHEST_PROTOCOL)
+
+    test(codeBook, histograms)
 
 
 if __name__ == '__main__':
